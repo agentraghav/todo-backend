78136068aa42c8b03834269e9036bb029b6863cb
agentraghav <raghvendrarathore2@gmail.com>
2021-03-19 14:59:21 +0530

    minor bug fix

--- src/storage/mongoose/todo.ts
+++ src/storage/mongoose/todo.ts
@@ -3,7 +3,7 @@ import { model, Schema } from 'mongoose';
 const todoSchema: Schema = new Schema(
   {
     title: { type: String, required: true },
-    isActive: { type: boolean, default: true },
+    isActive: { type: Boolean, default: true },
   },
   {
     collection: 'todos',
